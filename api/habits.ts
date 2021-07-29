2c5fc0a2578b7411235d1b29c7baa3bba1e7bd28
turtlebasket <turtlebasket@gmail.com>
2021-07-28 17:45:42 -0700

    Unfuck habit delete times again (shift deletion window back a day)

--- api/habits.ts
+++ api/habits.ts
@@ -97,8 +97,8 @@ export const habitHistoryAdd = async (id: string) => {
 export const habitHistoryRemove = async (id: string) => {
   const todayTD = new Date();
   const tzOffset = todayTD.getTimezoneOffset();
-  const today = new Date(new Date().setUTCHours(tzOffset/60, 0, 0, 0)).toISOString();
-  const tomorrow = new Date(new Date().setUTCHours(tzOffset/60+24, 0, 0, 0)).toISOString();
+  const today = new Date(new Date().setUTCHours(tzOffset/60-24, 0, 0, 0)).toISOString();
+  const tomorrow = new Date(new Date().setUTCHours(tzOffset/60, 0, 0, 0)).toISOString();
   console.log(`today:\t${today}`)
   console.log(`tomorrow:\t${tomorrow}`)
   const { data, error } = await supabase
